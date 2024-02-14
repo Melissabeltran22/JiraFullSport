ddbbdde80e7be3257494d2bb5d8bfcc93fee4329
Melissa Beltran <meli.cesde@gmail.com>
2024-02-14 17:31:01 -0500

    Scrum-25 Agregar base de datos

---
+++ database/Registros_FullSport.sql
@@ -0,0 +1,16 @@
+use FullSport
+
+select * from productos
+insert into productos values(001, 'Proteina Whey', 55000, 100, 'womenfit', 'proteina en polvo', 500, 'no aplica', 'no aplica')
+
+
+
+select * from pedidos
+insert into pedidos values('p0001', '2024-02-07')
+insert into pedidos values('p0002', '2024-02-06')
+insert into pedidos values('p0003', '2024-02-05')
+insert into pedidos values('p0004', '2024-02-04')
+insert into pedidos values('p0005', '2024-02-03')
+
+
+
